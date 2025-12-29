7694be121f6218bdab70054e6ec21ed5e14e5c4a
Gukil Kim <kki0287@naver.com>
2025-12-29 18:41:31 +0900

    Initial Commit

---
+++ WearWeather/WearWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearWeatherApp.swift
+//  WearWeather
+//
+//  Created by 김국일 on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct WearWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WearWeatherTests/WearWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WearWeatherTests.swift
+//  WearWeatherTests
+//
+//  Created by 김국일 on 12/29/25.
+//
+
+import Testing
+@testable import WearWeather
+
+struct WearWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
